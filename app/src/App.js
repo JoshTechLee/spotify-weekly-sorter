89277c0bdde652c188a2b8b6f741067812016882
joshjinlee <13jl45@gmail.com>
2021-05-05 18:31:54 -0400

    got the spotify callback working with the help of devtips

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,35 @@
 import './App.css';
 import SearchBox from './components/search_box/SearchBox';
-// import { spotifyAuthorization } from './resources/requests';
-// import { codeVerifier } from './resources/helper';
+import { spotifyAuthorization } from './resources/requests';
+import { codeVerifier } from './resources/helper';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect,
+    useHistory,
+    useLocation,
+} from 'react-router-dom';
 
 import { useSelector, useDispatch } from 'react-redux';
 
 function App() {
-    // const spotifyAuth = () => {
-    //     const code = codeVerifier();
-    //     spotifyAuthorization({ code }).then((res) => console.log(res));
-    // };
+    const spotifyAuth = () => {
+        const code = codeVerifier();
+        spotifyAuthorization({ code }).then((res) => console.log(res));
+    };
 
+    return <MainPage />;
+}
+
+export default App;
+
+const SpotifyLoginRoute = () => {
+    return <Route />;
+};
+
+const MainPage = () => {
     return (
         <div className="App grid-container">
             <div className="playlists-header">
@@ -18,10 +37,10 @@ function App() {
             </div>
             <div className="songs-header">I am song header</div>
             <div className="playlists-body">I am playlist body</div>
-            <div className="progress-bar"></div>
+            <div className="progress-bar">
+                <a href={process.env.REACT_APP_SERVER_ADDRESS + '/login'}>Login</a>
+            </div>
             <div className="songs-body"></div>
         </div>
     );
-}
-
-export default App;
+};
